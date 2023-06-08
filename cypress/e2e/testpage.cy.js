514e36d881c55dabfe5ceff596e81d08cca8ee63
Azri Mangsor <33415476+azrimangsor@users.noreply.github.com>
2023-06-08 08:10:45 +0800

    Update testpage.cy.js

--- cypress/e2e/testpage.cy.js
+++ cypress/e2e/testpage.cy.js
@@ -1,6 +1,6 @@
 describe('Sample Test', () => {
 
-    tag = 'PROJ-T001'
+    tag = 'PROJ-T1'
     it(`${tag} - Zephyr Jira Scale Integration Case`, () => {
         cy.visit('https://www.google.com/')
     })
